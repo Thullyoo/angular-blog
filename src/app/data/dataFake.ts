5a4e8b74508b1ca83c7ffad0062f16db5477df32
Thullyoo <thullyofilho@gmail.com>
2023-09-01 15:01:31 -0300

    change styles of content

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -3,24 +3,24 @@ export const dataFake = [
     "id": "1",
     "photo": "https://a.espncdn.com/combiner/i?img=%2Fphoto%2F2023%2F0826%2Fr1215940_2_1296x729_16%2D9.jpg&w=570&format=jpg",
     "title": "NOVO ATO NOVA MUNDANÇAS NOS MAPAS",
-    "description": "No novo ato do valorant episódio 7 ato 2, é lançado o novo mapa chamado Sunset e com ele vem a saída da rotação o mapa Pearl e Fracture, e o mapa Breeze retorna a rotação junto com o Sunset."
+    "description": "Sunset, o décimo mapa do Valorant, está oficialmente anunciado e chega ao jogo em 29 de agosto, com o lançamento do Episódio 7, Ato 2. O cenário é inspirado na cidade de Los Angeles, nos Estados Unidos, palco do Valorant Champions 2023, o campeonato mundial da modalidade."
   },
   {
     "id": "2",
     "photo": "https://s2-ge.glbimg.com/ySs5GjmYrxUW9x5yKyt2kPa6rVM=/1200x/smart/filters:cover():strip_icc()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2023/P/M/x8QhROQve2XX1GtAUvIA/valorant-itens-passe-batalha-ato-2-episodio-7.jpg",
     "title": "Novo passe de batalha",
-    "description": "noticabasdawdawd"
+    "description": "O novo passe de batalha do Episódio 7 Ato 2 do Valorant está chegando, trazendo novos conteúdos premium como skins, acessórios e Radianite Points (RPs). Custando 1.000 Valorant Points (VP), o passe dessa atualização do First-Person Shooter (FPS) da Riot Games trará três coleções de skins e uma Phantom canônica da agente Deadlock, de maio, e será lançado nesta terça-feira, 29 de agosto."
   },
   {
     "id": "3",
     "photo": "https://s2-ge.glbimg.com/Zd0AWy_n-caFXJZE-MGJe2UmIXk=/0x0:1648x804/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2023/O/t/FiR4lMTxiqv9t8dGKeMg/valorant-imperium-bundle.jpeg",
     "title": "Novo pacote imperium",
-    "description": "noticabasdawdawd"
+    "description": "A Riot Games revelou neste domingo a nova coleção de skins do Valorant, chamada Imperium. O pacote de cosméticos do First-Person Shooter (FPS) contará com quatro armas - Sheriff, Judge, Vandal e Operator - e uma faca. O lançamento será na terça-feira, 29 de agosto, com a chegada do Ato 2 Episódio 7, e custará 8.700 Valorant Points (VP)."
   },
   {
     "id": "4",
     "photo": "https://gamearena.gg/wp-content/uploads/2023/08/PREMIER-VALORANT.png",
     "title": "Premier voltando ao valorant",
-    "description": "noticabasdawdawd"
+    "description": "A Riot Games anunciou detalhes do lançamento do Premier no Valorant. O modo de torneios do First-Person Shooter (FPS) estará disponível em 29 de agosto, data do lançamento do Episódio 7, Ato 2, e será o caminho dos jogadores até o Challengers, o campeonato da segunda divisão do cenário competitivo."
   },
 ]
